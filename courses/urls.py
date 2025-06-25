5f72441b323cf7970ca4163f7c8dce7996c3235c
Mertcan Taş <mertcan.tas@outlook.com>
2025-06-25 16:25:29 +0300

    path converters added

--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from courses.views import kurslar, details
+from courses.views import kurslar, courseList, details, getCoursesByCategory, getCoursesByCategoryId
 
 urlpatterns = [
     path("", kurslar, name="kurslar"),
-    path("details/", kurslar, name="details"),
+    path("list", courseList, name="course-list"),
+    path("<str:kurs_adi>", kurslar, name="details"),
+    path("kategori/<str:category>", getCoursesByCategory, name="category"),
+    path("kategori/<int:category_id>", getCoursesByCategoryId, name="category"),
 ]
--- courses/views.py
+++ courses/views.py
@@ -4,5 +4,14 @@ from django.http import HttpResponse
 def kurslar(request):
     return HttpResponse("Kurslar")
 
-def details(request):
-    return HttpResponse("kurs detay")
+def courseList(request):
+    return HttpResponse("Kurs Listesi")
+
+def details(request, kurs_adi):
+    return HttpResponse(f"{kurs_adi} detay sayfasi")
+
+def getCoursesByCategory(request, category):
+    return HttpResponse(f"Kurs Kategorisi: {category}")
+
+def getCoursesByCategoryId(request, category_id):
+    return HttpResponse(category_id)
